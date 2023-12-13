86070292b90fbb15480b97d1a7838a540c90dd4a
Abhishek Singh <38339710+Abhishek7861@users.noreply.github.com>
2023-12-13 16:56:16 +0530

    Update axiosConfig.js

--- src/Apis/axiosConfig.js
+++ src/Apis/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://34.131.106.99:8000', // Replace with your API's base URL
+  baseURL: 'http://34.131.95.235:8000', // Replace with your API's base URL
   headers: {
     'Content-Type': 'application/json',
     // Add any other default headers here
